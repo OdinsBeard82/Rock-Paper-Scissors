c373d7cacb27c67d4ceb08d2b4c2d10bc2310bb7
Daniel Charles <danielcharles_500@hotmail.com>
2023-01-22 16:55:17 +0000

    more change

--- R-P-S.js
+++ R-P-S.js
@@ -1,3 +1,18 @@
-function getComputerChoice(); {
-   let randomChoice = Math.floor(Math.random() * getComputerChoice.length);
+const choices = ("rock", "paper", "scissors")
+function game(){ 
+
 }
+function computerChoice(){
+   return choices[Math.floor(Math.random() * choices.length)]
+}
+
+function playRound(){
+    const playerselection = 
+}
+function playerselection(){
+
+}
+function computerSelection(){
+
+}
+
