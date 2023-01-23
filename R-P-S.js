48b4c224827dce9b4818cacdee95c2d75cf2f421
Daniel Charles <danielcharles_500@hotmail.com>
2023-01-23 15:40:51 +0000

    new idea

--- R-P-S.js
+++ R-P-S.js
@@ -1,5 +1,43 @@
+function getComputerChoice() {
+    return (Math.floor(Math.random) * Choice);
+}
+const Choice("rock, paper, sicissors");
+console.log(choice);
 
- const choices = ["rock", "paper", "scissors"];
+//Your game is going to play against the computer, so begin 
+//with a function called getComputerChoice that will randomly 
+//return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this 
+//function in the game to make the computer’s play. Tip: use 
+//the console to make sure this is returning the expected 
+//output before moving to the next step!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const choices = ["rock", "paper", "scissors"];
 
  function game() {
     playRound();
@@ -12,9 +50,20 @@ function playRound() {
 
   function PlayerChoice() {
    let input = prompt("Tyoe Rock, Paper, or Scissors");
+   while (input == null) {
+    input = prompt("Type Rock, Paper, or Scissors");
+  }
    input = input.toLowerCase();
+   let check = ValidateInput(input);
+   while (check == false {
+       prompt("Type Rock, Paper, or Scissors. Spelling needs to be exact, but capitilization doesn't matter")
+   } 
   }
 
   function ComputerChoice(){
     return choices[Math.floor(Math.random() * choices.length)]
   }
+
+  function ValidateInput(choice) {
+    return choices.includes(choice);
+    }
