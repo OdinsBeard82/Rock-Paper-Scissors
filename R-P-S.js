1cbd454b2f84ac51f8b49db2995cc4de9f13dcf0
Daniel Charles <danielcharles_500@hotmail.com>
2023-01-24 13:03:39 +0000

    and pause

--- R-P-S.js
+++ R-P-S.js
@@ -1,19 +1,27 @@
 function getComputerChoice() {
-    return (Math.floor(Math.random) * Choice);
+    return (Math.floor(Math.random) * choice);
 }
-const Choice("rock, paper, sicissors");
-console.log(choice);
+const choice=("rock, paper, sicissors");
 
-    var userchoice = prompt ("Choose rock, papaer or scissors");
-    var ComputerChoice = Math.random();
+function playRound(playerSelection, computerSelection) {
+    var userchoice = prompt ("Choose rock, paper or scissors");
+    var computerChoice = Math.random();
     if (computerChoice < 0.34) {
         computerChoice = "rock";
     } else if(computerChoice <= 0.67) {
         computerChoice = "paper";
     } else {
         computerChoice = "scissors;"
-    
+    }
+
+    if (userchoice == "rock") {
+    if (computerChoice == "paper") {
+    }
+    }
     }
     const playerSelection ="rock";
-    const computerSelection = getCOmputerChoice();
-     
+    const computerSelection = getComputerChoice();
+
+
+    console.log(playRound(playerSelection, computerSelection));
+
