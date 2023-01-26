6ff83b5745bcc0dd3d216d48207b3d98a0af68f4
Daniel Charles <danielcharles_500@hotmail.com>
2023-01-26 12:27:31 +0000

    ALmost there, something is not linking

--- R-P-S.js
+++ R-P-S.js
@@ -1,59 +1,52 @@
-//function getComputerChoice() {
-   // return (Math.floor(Math.random) * choice);
-//}
 const resultDisplay = document.querySelector('#result')
-// this grabs the result id from index.html
 const choicesDisplay = document.querySelector('#choices')
-//This grabs the choices id from index.ht;l
+const choices = ["rock, paper, scissors"];
 
 const handleClick = (e) => {
-    //this asignes an action for the handleClick
-    getResults(e.target.innerHTML, choices[Math.floor(Match.random() * choices.length)])
-    //result of what user clicks and result of computers random choice using math random
+    getResults(e.target.innerHTML, choices[Math.floor(Math.random() * choices.length)])
+
 }
-const choices = ["rock, paper, sicissors"];
-//Asigns choices as rock, paper and scissors
+
 choices.forEach(choice => {
     const button = document.createElement('button')
-    //this creates a button for the choices
-    button.innerHTML = choice
-    //grab the button and you html to asign it the choice
+    button.innerHTML= choice
     button.addEventListener('click', handleClick)
-    //grab the button and add an eventListener to listen 
-    //out for a click and then call the handleClick functcion
     choicesDisplay.appendChild(button)
-    //grab choices display and use appendChild button
 })
 
 const getResults = (userChoice, computerChoice) => {
-   switch(userChoice + computerChoice ) {
-       case 'scissorspaper':
-       case 'rockscissors':
-       case 'paperrock':
-       resultDisplay.innerHTML = 'You chose' + userChoice + 'the computer chose ' + computerChoice + ' , you win';
-       break
-
-// you chose computer choice and computer chose computer choice which means you win
-   }
+   switch (userChoice + computerChoice) {
+   case 'scissorspaper':
+   case 'rockscissors':
+   case 'paperrock':
+      resultDisplay.innerHTML = 'You chose' + userChoice + 'the computer chose ' + computerChoice + ' , you win'
+      break
+   case 'paperscissors':
+   case 'scissorsrock':
+   case 'rockpaper':
+      resultDisplay.innerHTML = 'You chose' + userChoice + 'the computer chose ' + computerChoice + ' , you lose'
+      break
+   case 'scissorsscissors':
+   case 'rockrock':
+   case 'paperpaper':
+      resultDisplay.innerHTML = 'You chose' + userChoice + 'the computer chose ' + computerChoice + ' , draw'
+      break
+   }   
 }
-//function playRound(playerSelection, computerSelection) {
- //   var userchoice = prompt ("Choose rock, paper or scissors");
- //   var computerChoice = Math.random();
-   // if (computerChoice < 0.34) {
- //       computerChoice = "rock";
- //   } else if(computerChoice <= 0.67) {
- //       computerChoice = "paper";
- //   } else {
- //       computerChoice = "scissors;"
- //   }
- //   if (userchoice == "rock") {
- //   if (computerChoice == "paper") {
- //   }
-  //  }
-  //  }
-  //  const playerSelection ="rock";
- //   const computerSelection = getComputerChoice();
 
 
+// this grabs the result id from index.html
+//This grabs the choices id from index.html
+//Asigns choices as rock, paper and scissors
 
+//this asignes an action for the handleClick
+//result of what user clicks and result of computers random choice using math random
+//Asigns choices as rock, paper and scissors
 
+ //this creates a button for the choices
+ //grab the button and you html to asign it the choice
+
+//grab the button and add an eventListener to listen 
+//out for a click and then call the handleClick functcion
+//grab choices display and use appendChild button
+// you chose computer choice and computer chose computer choice which means you win
