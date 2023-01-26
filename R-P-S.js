dac762b3a21769b05acfcdb0dd0546b7ab8d2787
Daniel Charles <danielcharles_500@hotmail.com>
2023-01-26 09:49:51 +0000

    A new path

--- R-P-S.js
+++ R-P-S.js
@@ -1,27 +1,38 @@
-function getComputerChoice() {
-    return (Math.floor(Math.random) * choice);
+//function getComputerChoice() {
+   // return (Math.floor(Math.random) * choice);
+//}
+const resultDisplay = document.querySelector('#result')
+const choicesDisplay = document.querySelector('#choices')
+
+const handleClick = () => {
+    console.log('clicked')
 }
-const choice=("rock, paper, sicissors");
+const choices = ["rock, paper, sicissors"];
+choices.forEach(choice => {
+    const button = document.createElement('button')
+    button.innerHTML = choice
+    button.addEventListener('click', handleClick)
+    choicesDisplay.appendChild(button)
+})
 
-function playRound(playerSelection, computerSelection) {
-    var userchoice = prompt ("Choose rock, paper or scissors");
-    var computerChoice = Math.random();
-    if (computerChoice < 0.34) {
-        computerChoice = "rock";
-    } else if(computerChoice <= 0.67) {
-        computerChoice = "paper";
-    } else {
-        computerChoice = "scissors;"
-    }
+//function playRound(playerSelection, computerSelection) {
+ //   var userchoice = prompt ("Choose rock, paper or scissors");
+ //   var computerChoice = Math.random();
+   // if (computerChoice < 0.34) {
+ //       computerChoice = "rock";
+ //   } else if(computerChoice <= 0.67) {
+ //       computerChoice = "paper";
+ //   } else {
+ //       computerChoice = "scissors;"
+ //   }
+ //   if (userchoice == "rock") {
+ //   if (computerChoice == "paper") {
+ //   }
+  //  }
+  //  }
+  //  const playerSelection ="rock";
+ //   const computerSelection = getComputerChoice();
 
-    if (userchoice == "rock") {
-    if (computerChoice == "paper") {
-    }
-    }
-    }
-    const playerSelection ="rock";
-    const computerSelection = getComputerChoice();
 
 
-    console.log(playRound(playerSelection, computerSelection));
 
