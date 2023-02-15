e06a131f66ed981970d42f00a8e127c01ace058a
OdinsBear82 <danielcharles_500@hotmail.com>
2023-02-15 13:29:41 +0000

    This is finished

--- R-P-S.js
+++ R-P-S.js
@@ -1,60 +1,127 @@
-const resultDisplay = document.querySelector('#result')
-const choicesDisplay = document.querySelector('#choices')
-const choices1Display = document.querySelector('#choices1')
+const resultDisplay = document.querySelector("#result");
+const choicesDisplay = document.querySelector("#choices");
 const choices = ["rock", "paper", "scissors"];
+const playButton = document.createElement("button");
+
+let roundCount = 0;
 
 const handleClick = (e) => {
-    getResults(e.target.innerHTML, getComputerChoice())
+  if (roundCount < 5) {
+    roundCount++;
+    getResults(e.target.innerHTML, getComputerChoice());
+
+    if (roundCount === 5) {
+      disableButtons();
+    }
+  }
+};
+
+function disableButtons() {
+  const buttons = document.querySelectorAll("#choices button");
+  buttons.forEach((button) => {
+    button.disabled = true;
+  });
 }
 
-const getComputerChoice = () => {
-   return choices [Math.floor(Math.random() * choices.length)];
+function getComputerChoice() {
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
-choices.forEach(choice => {
-    const button = document.createElement('button')
-    button.innerHTML= choice
-    button.addEventListener('click', handleClick)
-    choicesDisplay.appendChild(button)
-})
-
+choices.forEach((choice) => {
+  const button = document.createElement("button");
+  button.innerHTML = choice;
+  button.addEventListener("click", handleClick);
+  choicesDisplay.appendChild(button);
+});
 
 const getResults = (playerSelection, computerSelection) => {
-   const result = playRound(playerSelection, computerSelection);
-   resultDisplay.innerHTML = result;
-}
-   function playRound(playerSelection, computerSelection) {
-   switch (playerSelection + computerSelection) {
-   case 'scissorspaper':
-   case 'rockscissors':
-   case 'paperrock':
-      resultDisplay.innerHTML = 'You chose' + playerSelection + 'the computer chose ' + computerSelection + ' , you win'
-      break
-   case 'paperscissors':
-   case 'scissorsrock':
-   case 'rockpaper':
-      resultDisplay.innerHTML = 'You chose' + playerSelection + 'the computer chose ' + computerSelection + ' , you lose'
-      break
-   case 'scissorsscissors':
-   case 'rockrock':
-   case 'paperpaper':
-      resultDisplay.innerHTML = 'You chose' + playerSelection + 'the computer chose ' + computerSelection + ' , draw'
-      break
-   }   
-}
-const game = () => { 
-   let playerScore = 0; 
-   let computerScore = 0; 
-   for (let i = 0; i < 5; i++) { 
-      const playerSelection = getComputerChoice(); 
-      const computerSelection = getComputerChoice(); 
-      const result = playRound(playerSelection, computerSelection);
-      if (result.includes("win")) { playerScore++; } 
-      else if (result.includes("lose")) { computerScore++; } } 
-      if (playerScore > computerScore) { resultDisplay.innerHTML = "You won the game! Score: " + playerScore + " - " + computerScore; } 
-      else if (playerScore < computerScore) { resultDisplay.innerHTML = "You lost the game. Score: " + playerScore + " - " + computerScore; } 
-      else { resultDisplay.innerHTML = "The game was a draw. Score: " + playerScore + " - " + computerScore; } }
+  const result = playRound(playerSelection, computerSelection);
+  resultDisplay.innerHTML = result;
+};
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice(); 
-console.log(playRound(playerSelection, computerSelection));
+function playRound(playerSelection, computerSelection) {
+  switch (playerSelection + computerSelection) {
+    case "scissorspaper":
+    case "rockscissors":
+    case "paperrock":
+      return (
+        "You chose " +
+        playerSelection +
+        ", the computer chose " +
+        computerSelection +
+        ", you win"
+      );
+    case "paperscissors":
+    case "scissorsrock":
+    case "rockpaper":
+      return (
+        "You chose " +
+        playerSelection +
+        ", the computer chose " +
+        computerSelection +
+        ", you lose"
+      );
+    case "scissorsscissors":
+    case "rockrock":
+    case "paperpaper":
+      return (
+        "You chose " +
+        playerSelection +
+        ", the computer chose " +
+        computerSelection +
+        ", draw"
+      );
+  }
+}
+
+const game = () => {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = getComputerChoice();
+    const computerSelection = getComputerChoice();
+    const result = playRound(playerSelection, computerSelection);
+
+    if (result.includes("win")) {
+      playerScore++;
+    } else if (result.includes("lose")) {
+      computerScore++;
+    }
+
+    if (playerScore === 3 || computerScore === 3) {
+      break;
+    }
+  }
+
+  if (playerScore > computerScore) {
+    resultDisplay.innerHTML =
+      "You won the game! Score: " + playerScore + " - " + computerScore;
+  } else if (playerScore < computerScore) {
+    resultDisplay.innerHTML =
+      "You lost the game. Score: " + playerScore + " - " + computerScore;
+  } else {
+    resultDisplay.innerHTML =
+      "The game was a draw. Score: " + playerScore + " - " + computerScore;
+  }
+
+  // reset the game after 5 rounds
+  setTimeout(() => {
+    playerScore = 0;
+    computerScore = 0;
+    resultDisplay.innerHTML = "";
+    roundCount = 0;
+    enableButtons();
+  }, 2000);
+};
+
+function enableButtons() {
+  const buttons = document.querySelectorAll("#choices button");
+  buttons.forEach((button) => {
+    button.disabled = false;
+  });
+}
+
+playButton.innerHTML = "Play Game";
+playButton.addEventListener("click", game);
+document.body.appendChild(playButton);
